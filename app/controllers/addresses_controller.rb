396d9132b95d97ed5c7dc9f5ecd32b2026b82ad2
Patrick Kim <pkim050@gmail.com>
2019-05-10 07:46:47 +0000

    Done.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,35 @@
 class AddressesController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def adress_params
+    params.require(:address).permit(:street_address_1, :street_address_2, :city, :state, :zipcode, :address_type)
+  end
 end
